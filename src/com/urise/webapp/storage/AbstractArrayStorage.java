aec818135aee61ba23352eecec64600c0ebdcfcb
Anton Alexeev <gonza_ant@mail.ru>
2025-10-21 11:45:30 +0300

    made use of the Template Method pattern and complicit abstract class AbstractArrayStorage

--- src/com/urise/webapp/storage/AbstractArrayStorage.java
+++ src/com/urise/webapp/storage/AbstractArrayStorage.java
@@ -1,7 +1,11 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.exception.ExistResumeException;
+import com.urise.webapp.exception.FullStorageArrayException;
 import com.urise.webapp.exception.NotExistResumeException;
 import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
 
 public abstract class AbstractArrayStorage implements Storage {
     protected static final int STORAGE_LIMIT = 10000;
@@ -13,6 +17,46 @@ public abstract class AbstractArrayStorage implements Storage {
         return size;
     }
 
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void update(Resume r) {
+        int index = findResumeIndex(r.getUuid());
+        if (index < 0) {
+            throw new NotExistResumeException("Ошибка: резюме (" + r.getUuid() + ") не существует, обновление не возможно.");
+        }
+        storage[index] = r;
+        System.out.println("Резюме (" + storage[index] + ") успешно обновлено.");
+    }
+
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    public void save(Resume r) {
+        if (size >= STORAGE_LIMIT) {
+            throw new FullStorageArrayException("Ошибка: база данных заполнена, сохранение не возможно.");
+        }
+        int index = findResumeIndex(r.getUuid());
+        if (index >= 0) {
+            throw new ExistResumeException("Ошибка: резюме (" + r.getUuid() + ") уже существует, сохранение не возможно.");
+        }
+        insertElement(r, index);
+        size++;
+    }
+
+    public void delete(String uuid) {
+        int index = findResumeIndex(uuid);
+        if (index < 0) {
+            throw new NotExistResumeException("Ошибка: резюме (" + uuid + ") не существует, удаление не возможно.");
+        }
+        System.arraycopy(storage, index + 1, storage, index, size - index - 1);
+        storage[size] = null;
+        size--;
+    }
+
     public Resume get(String uuid) {
         int index = findResumeIndex(uuid);
         if (index < 0) {
@@ -22,5 +66,7 @@ public abstract class AbstractArrayStorage implements Storage {
     }
 
     protected abstract int findResumeIndex(String uuid);
+
+    protected abstract void insertElement(Resume r, int index);
 }
 
--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -1,64 +1,22 @@
 package com.urise.webapp.storage;
 
-import com.urise.webapp.exception.ExistResumeException;
-import com.urise.webapp.exception.FullStorageArrayException;
-import com.urise.webapp.exception.NotExistResumeException;
 import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
 
 public class SortedArrayStorage extends AbstractArrayStorage{
 
-    @Override
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
-    @Override
-    public void update(Resume r) {
-        int index = findResumeIndex(r.getUuid());
-        if (index < 0) {
-            throw new NotExistResumeException("Ошибка: резюме (" + r.getUuid() + ") не существует, обновление не возможно.");
-        }
-        storage[index] = r;
-        System.out.println("Резюме (" + storage[index] + ") успешно обновлено.");
-    }
-
-    @Override
-    public void save(Resume r) {
-        if (size >= STORAGE_LIMIT) {
-            throw new FullStorageArrayException("Ошибка: база данных заполнена, сохранение не возможно.");
-        }
-        int index = findResumeIndex(r.getUuid());
-        if (index >= 0) {
-            throw new ExistResumeException("Ошибка: резюме (" + r.getUuid() + ") уже существует, сохранение не возможно.");
-        }
-        int insertIndex = -index - 1;
-        System.arraycopy(storage, insertIndex, storage, insertIndex + 1, size - insertIndex);
-        storage[insertIndex] = r;
-        size++;
-    }
-
-    @Override
-    public void delete(String uuid) {
-        int index = findResumeIndex(uuid);
-        if (index < 0) {
-            throw new NotExistResumeException("Ошибка: резюме (" + uuid + ") не существует, удаление не возможно.");
-        }
-        System.arraycopy(storage, index + 1, storage, index, size - index - 1);
-        storage[--size] = null;
-    }
-
-    @Override
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
-    }
-
-    @Override
+       @Override
     protected int findResumeIndex(String uuid) {
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
         return Arrays.binarySearch(storage,0, size, searchKey);
+    }
+
+    @Override
+    protected void insertElement(Resume r, int index) {
+        int insertIndex = -index - 1;
+        System.arraycopy(storage, insertIndex, storage, insertIndex + 1, size - insertIndex);
+        storage[insertIndex] = r;
     }
 }
