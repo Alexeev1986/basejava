daca6edc6922a24b2f593031a147d51c7f00be1c
Anton Alexeev <gonza_ant@mail.ru>
2025-10-24 13:41:52 +0300

    create class MainReflection, create testing from JUnit4 and JUnit5

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -1,10 +1,9 @@
 package com.urise.webapp;
 
 import com.urise.webapp.model.Resume;
-
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Field;
 
 public class MainReflection {
     public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
@@ -15,9 +14,24 @@ public class MainReflection {
         System.out.println(field.get(r));
         field.set(r, "new uuid");
         System.out.println(r);
+
         System.out.println("Вызов метода - toString через Field ");
         Method toStringMethod = r.getClass().getMethod("toString");
-        Object result = toStringMethod.invoke(r);
-        System.out.println(result);
+        Object runStrMet = toStringMethod.invoke(r);
+        System.out.println(runStrMet);
+
+       /* String str = " ";
+        Field[] fields = str.getClass().getDeclaredFields();
+        System.out.println("Fields from String:");
+        for (Field field2 : fields) {
+            System.out.println(field2);
+        }
+        Method[] methods = str.getClass().getMethods();
+        System.out.println("Methods from String:");
+        for (Method method : methods){
+            System.out.println(method);
+        }
+        Object stringHashCose = methods[3].invoke("WatsUP!");
+        System.out.println(stringHashCose);*/
     }
 }
