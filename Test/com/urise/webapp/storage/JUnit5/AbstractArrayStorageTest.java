6ff49079788858e84b8c8e54324df73a33c3e9cf
Anton Alexeev <gonza_ant@mail.ru>
2025-10-29 10:11:47 +0300

    corrected errors

---
+++ Test/com/urise/webapp/storage/JUnit5/AbstractArrayStorageTest.java
@@ -0,0 +1,10 @@
+package com.urise.webapp.storage.JUnit5;
+
+import com.urise.webapp.storage.Storage;
+
+public abstract class AbstractArrayStorageTest extends AbstractStorageTest {
+
+    protected AbstractArrayStorageTest(Storage storage) {
+        super(storage);
+    }
+}
