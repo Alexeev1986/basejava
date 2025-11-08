06bcff7d68200f6280869d6b1979cce88c8701c3
Anton Alexeev <gonza_ant@mail.ru>
2025-11-08 16:52:42 +0300

    corrected code duplication in the doDelete methods array-related

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -16,13 +16,6 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     }
 
     @Override
-    protected void doDelete(Integer index) {
-        System.arraycopy(storage, index + 1, storage, index, size - index - 1);
-        storage[size] = null;
-        size--;
-    }
-
-    @Override
     protected void insertElement(Resume r, Integer index) {
         int insertIndex = -(Integer) index - 1;
         System.arraycopy(storage, insertIndex, storage, insertIndex + 1, size - insertIndex);
