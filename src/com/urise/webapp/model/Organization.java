07bdee5355a85cab4837f0d83ab33df58bf728dc
Anton Alexeev <gonza_ant@mail.ru>
2025-11-08 19:20:04 +0300

    Added nested static class Link to the class Organization.

--- src/com/urise/webapp/model/Organization.java
+++ src/com/urise/webapp/model/Organization.java
@@ -4,31 +4,23 @@ import java.util.Collections;
 import java.util.List;
 
 public class Organization {
-    private final String name;
-    private final String url;
+    private final Link link;
     private final List<Position> positions;
 
     public Organization(String name, String url, List<Position> positions) {
-        this.name = name;
-        this.url = url;
+        this.link = new Link(name, url);
         this.positions = List.copyOf(positions);
     }
 
     public Organization(String name, String url, Position position) {
-        this.name = name;
-        this.url = url;
+        this.link = new Link(name, url);
         this.positions = Collections.singletonList(position);
 
     }
 
-    public String getName() {
-        return name;
+    public Link getLink() {
+        return link;
     }
-
-    public String getUrl() {
-        return url;
-    }
-
     public List<Position> getPeriods() {
         return positions;
     }
@@ -38,12 +30,34 @@ public class Organization {
         StringBuilder sb = new StringBuilder();
         String prefix = "-".repeat(25);
 
-        sb.append(prefix).append(name);
-        if (url != null && !url.isEmpty()) {
-            sb.append(" [").append(url).append("]");
-        }
-        sb.append("\n");
+        sb.append(prefix).append(link).append("\n");
         sb.append(positions.toString());
         return sb.toString();
+    }
+
+    public static class Link {
+        private final String name;
+        private final String url;
+
+        public Link(String name, String url) {
+            this.name = name;
+            this.url = url;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        @Override
+        public String toString() {
+            if (url != null && !url.isBlank()) {
+                return name + ": " + url;
+            }
+            return name;
+        }
     }
 }
